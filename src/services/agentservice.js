27818266b25e9133b6e0ab6e5e2be7918b151255
Rayable <rayable11.0808@gmail.com>
2026-03-09 13:29:11 +0800

    Added RAG service implementation in agentservice.js

--- src/services/agentservice.js
+++ src/services/agentservice.js
@@ -1,16 +1,18 @@
 import memoryservice from  "./memoryservice.js";
+import ragservice from "./ragservice.js";
 import anthropic from "../config/anthropic.js";
 const executeagent = async (message,sessionId) => {
     const history = memoryservice.getHistory(sessionId);
+    const context = await ragservice.getcontext(message);
+
+    const  systemprompt = `You are an AI assistant designed to answer queries on your programmer.Use this context if relevant: ${context}`;
 
     memoryservice.saveMessage(sessionId, "user",message);
 
     const response = await anthropic.messages.create({
         model: "claude-3-5-haiku-20241022",
         max_tokens: 1000,
-        messages: [
-            { role: "user", content: message},
-        ],
+        messages: history
     });
     const reply = response.content[0].text;
 
